d9ab4c526d83cc51fc39df62ab32abe76fdce978
2dam <you@example.com>
2022-09-16 10:48:55 +0200

    Branch changed. Model is completed, view is yet to come

--- src/model/dao/DBModel.java
+++ src/model/dao/DBModel.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-import com.mysql.cj.protocol.SocksProxySocketFactory;
-
 import model.Model;
 
 public class DBModel implements Model {
